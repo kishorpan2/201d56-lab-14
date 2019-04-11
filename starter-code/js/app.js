23416a35e9e8b4a3375a205370c512ee394991eb
Rob Bronson <robert.j.bronson+cf@gmail.com>
2019-04-11 16:41:23 -0700

    drove while Kishor navigated.  Added handle submit functions

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -14,6 +14,8 @@ Cart.prototype.addItem = function(product, quantity) {
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  //Done
+  localStorage.setItem('merchandise', JSON.stringify(this.items));
 };
 
 Cart.prototype.removeItem = function(item) {
